e3faf3e59b05e125b32e55c521311826382e6d47
zhaoqian2016 <1182516485@qq.com>
2016-06-29 09:20:54 +0800

    xing

---
+++ 12/hello.js
@@ -0,0 +1,37 @@
+// alert("Hi");
+// ddfdfdf
+/*  ddfdfdf
+dfdfdf
+
+*/
+
+// // 定义变量
+// var age = 18;
+// // alert(age);
+// console.log(age);
+
+// {
+//     var s1 = "西北" + "民大";
+//     console.log(s1);
+// }
+
+// if (age > 35) {
+//     console.log("大叔");
+// } else {
+//     console.log("小弟");
+// }
+
+// for (var i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+function say(age) {
+    if (age > 35) {
+        console.log("大叔");
+    } else {
+        console.log("小弟");
+    }
+}
+
+say(40);
